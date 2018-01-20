078f8222db10d08439108f2660a16d5b877c00bc
fan <fanear233@gmail.com>
2018-01-21 01:38:08 +0800

    修复抓取输入流为乱码的BUG

--- src/fan/core/Tools.java
+++ src/fan/core/Tools.java
@@ -9,12 +9,12 @@ public class Tools {
 	final static String broadbandName = "";//宽带名称 例如：“宽带连接”
 	final static String account = "";//宽带账号 例如：“1700500xxxxx”
 	final static String passWord = "";//宽带密码
-	final static String webSite = "";//用来ping的网址，检测网通不通
+	final static String webSite = "www.baidu.com";//用来ping的网址，检测网通不通
 	
 	public void Connect() throws IOException {
 
 		Process pro = Runtime.getRuntime().exec("Rasdial" + " " + broadbandName + " " + account + " " + passWord);
-		InputStreamReader in = new InputStreamReader(pro.getInputStream());
+		InputStreamReader in = new InputStreamReader(pro.getInputStream(),"GBK");
 		BufferedReader bufferedReader = new BufferedReader(in);
 		String line = null;
 		while ((line = bufferedReader.readLine()) != null) {
@@ -35,15 +35,21 @@ public class Tools {
 	public boolean ping() throws IOException {
 		
 		Process pro = Runtime.getRuntime().exec("ping"+" "+webSite);
-		InputStreamReader in = new InputStreamReader(pro.getInputStream());
+		InputStreamReader in = new InputStreamReader(pro.getInputStream(),"GBK");
 		BufferedReader bufferedReader = new BufferedReader(in);
 		String line = null;
 		while ((line = bufferedReader.readLine()) != null) {
 			if(line.contains("Ping 请求找不到主机")){
+				System.out.println("网络不通，正在尝试联网！");
 				shutDown();
 				Connect();
 			}else if(line.contains("正在 Ping")){
+				System.out.println("宽带已连接！");
+				Connect();
 				return true;
+			}else if(line.contains("用法: ping")){
+				System.out.println("未填写网址！");
+				System.exit(1);
 			}
 		}
 		return false;
