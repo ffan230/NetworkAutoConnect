f46122bffaf5613111f023cc7ac45752457cb3e8
fan <fanear233@gmail.com>
2018-01-21 01:57:04 +0800

    修复未输入宽带信息造成程序死循环的BUG

--- src/fan/core/Run.java
+++ src/fan/core/Run.java
@@ -2,20 +2,24 @@ package fan.core;
 
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
 public class Run {
 
 	public static void main(String[] args) {
 		try {
 			Tools p = new Tools();
-			while(true){
-				if(p.ping()){
+			while (true) {
+				if (p.ping()) {
 					System.exit(1);
-				}else{
-					
+				} else {
+					JOptionPane.showMessageDialog(null, "请检查Tools中宽带信息是否填写！", "错误", JOptionPane.ERROR_MESSAGE);
+					System.exit(1);
 				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
 	}
 }
